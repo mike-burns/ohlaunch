1c5f5cc6e3ff7bfbfe1dda9bfbd19d8338274e0f
Mike Burns <mburns@thoughtbot.com>
2011-09-16 15:20:48 -0400

    Start converting the Java to Scala. Broken for now.

--- src/main/scala/LaunchActivity.scala
+++ src/main/scala/LaunchActivity.scala
@@ -1,179 +1,211 @@
 package com.mike_burns.ohlaunch
 
 import android.app.Activity
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.content.pm.ResolveInfo
-
-import android.view.GestureDetector
-import android.view.GestureDetector.SimpleOnGestureListener
-import android.widget.Toast
+import android.os.Parcelable
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
 import android.view.View
-import android.view.View.OnTouchListener
-import android.view.MotionEvent
+import android.widget.TextView
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.app.ListFragment
+import android.view.LayoutInflater
+import android.widget.ArrayAdapter
+import android.view.ViewGroup
+
+import android.R.layout
+
+import java.util.Collections
+import android.content.pm.ResolveInfo
+import java.util.List
+import android.content.pm.PackageManager
+import android.content.Intent
+import java.util.ArrayList
+import android.widget.TableRow
+import android.widget.TableLayout
+import android.widget.ImageView
+import android.os.AsyncTask
+import android.view.ViewTreeObserver
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
 
 import android.util.Log
 
-import android.widget.TextView
-import android.widget.ImageView
-import android.view.ViewGroup
-import android.content.Context
+class PaginatingActivity extends FragmentActivity {
+  val awesomePager : ViewPager = null
 
-import android.util.DisplayMetrics
-
-import android.widget.AdapterView
-import android.widget.AdapterView.OnItemClickListener
-import android.content.ComponentName
-
-import android.content.Intent
-
-import android.os.Debug
-
-class LaunchActivity extends Activity with AsyncPackages with TypedActivity {
-  var allPackages = List[ResolveInfo]()
-  var numberOfPackages = 0
-  var offset = 0
-  var adapter = null : ArrayAdapter[ResolveInfo]
-
-  override def onCreate(savedInstanceState: Bundle) {
+  override def onCreate(savedInstanceState : Bundle) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.paginated_activity_list)
 
-    val list = findView(TR.list)
+    setContentView(R.layout.main)
+    awesomePager = findViewById(R.id.paginatorizer).asInstanceOf[ViewPager]
 
-    adapter = new PackageAdapter(this)
-    list.setAdapter(adapter)
-
-    val gestureDetector = new GestureDetector(
-      new LeftRightDetector().
-        onLeft({ () => flingLeft}).
-        onRight({ () => flingRight})
-    )
-    list.setOnTouchListener(new View.OnTouchListener {
-      def onTouch(v : View, event : MotionEvent) = {
-        gestureDetector.onTouchEvent(event)
-        false
+    vto = awesomePager.getViewTreeObserver()
+    vto.addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+      override def onGlobalLayout {
+        new PackageLookUpper(awesomePager, getPackageManager()).execute(1)
       }})
-
-    list.setOnItemClickListener(new OnItemClickListener {
-      def onItemClick(parent : AdapterView[_], v : View, position : Int, id : Long) {
-        val item = parent.getItemAtPosition(position).asInstanceOf[ResolveInfo]
-  
-        val intent = new Intent
-        intent.setClassName(item.activityInfo.applicationInfo.packageName,
-          item.activityInfo.name)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        intent.setAction(Intent.ACTION_MAIN)
-        intent.addCategory(Intent.CATEGORY_LAUNCHER)
-
-        startActivity(intent)
-      }})
-
-    val spinner = findView(TR.spinner)
-
-    withPackages(showApps(spinner)).butFirst { () =>
-      Debug.startMethodTracing
-      spinner.setVisibility(View.VISIBLE)
-    }.go
   }
 
-  private def showApps(spinner : View)(apps : List[ResolveInfo]) {
-    allPackages = apps
-    numberOfPackages = apps.length
-    apps.take(perPage).foreach(adapter.add(_))
-    spinner.setVisibility(View.GONE)
-    Debug.stopMethodTracing
-  }
+  class PackageLookUpper extends AsyncTask[Void, Void, List[ResolveInfo]] {
+    val pager : ViewPager = null
+    val packageManager : PackageManager = null
+    val width = 1
+    val height = 1
 
-  private def perPage = {
-    val heightPixels = findView(TR.list).getHeight
+    def this(pager : ViewPager, packageManager : packageManager) = {
+      super()
+      this.pager = pager
+      this.packageManager = packageManager
+      this.height = pager.getHeight
+      this.width = pager.getWidth
+    }
 
-    val perRow = 4
-    val cellContentHeight = 88
-    val padding = 10
-    val paddingTop = 3
-    val fudge = 10
+    override def doInBackground(ignored : Void...) : List[ResolveInfo] = {
+      val mainIntent = new Intent(Intent.ACTION_MAIN, null)
+      mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
 
-    val totalCellHeight = cellContentHeight + padding + paddingTop// + fudge
+      val resolveInfos = packageManager.queryIntentActivities(mainIntent, 0)
+      Collections.sort(resolveInfos, new ResolveInfo.DisplayNameComparator(packageManager))
 
-    (heightPixels / totalCellHeight) * perRow
-  }
+      resolveInfos
+    }
 
-  private def flingLeft {
-    offset = List(offset + perPage, lastPageOffset).min
-    adapter.clear
-    allPackages.slice(offset, offset+perPage).foreach(adapter.add(_))
-  }
-
-  private def flingRight {
-    offset = List(offset - perPage, 0).max
-    adapter.clear
-    allPackages.slice(offset, offset+perPage).foreach(adapter.add(_))
-  }
-
-  private def pageCount = {
-    val count = numberOfPackages / perPage
-    if (numberOfPackages % perPage > 0)
-      count + 1
-    else
-      count
-  }
-
-  private def lastPageOffset = {
-    (perPage * (numberOfPackages.asInstanceOf[Float] / perPage).ceil - perPage).asInstanceOf[Int]
-  }
-
-  class PackageAdapter(activity : Activity) extends ArrayAdapter[ResolveInfo](activity.asInstanceOf[Context], R.layout.activity_item, R.id.app_name) {
-    override def getView(position : Int, convertView : View, parent : ViewGroup) = {
-      val inflater = activity.getLayoutInflater
-      val packageManager = activity.getPackageManager
-      val cell = inflater.inflate(R.layout.activity_item, parent, false)
-
-
-      val item = getItem(position).asInstanceOf[ResolveInfo]
-      val iconView = cell.findViewById(R.id.app_icon).asInstanceOf[ImageView]
-      iconView.setImageDrawable(item.loadIcon(packageManager))
-      val nameView = cell.findViewById(R.id.app_name).asInstanceOf[TextView]
-      nameView.setText(item.loadLabel(packageManager))
-
-      cell
+    override def onPostExecute(resolveInfos : List[ResolveInfo]) {
+      val awesomeAdapter = new AwesomePagerAdapter(
+          getSupportFragmentManager,
+          resolveInfos,
+          this.height / 88,
+          this.width / 78);
+      pager.setAdapter(awesomeAdapter)
     }
   }
 
-  class LeftRightDetector extends SimpleOnGestureListener {
-    var leftCallback = { () => () }
-    var rightCallback = { () => () }
+  class AwesomePagerAdapter extends FragmentPagerAdapter {
+    val resolveInfos : List[ResolveInfo] = null
+    val numRows = 1
+    val numCols = 1
 
-    override def onFling(event1 : MotionEvent, event2 : MotionEvent, xVelocity : Float, yVelocity : Float) = {
-      if (leftFling(event1, event2, xVelocity, yVelocity)) {
-        leftCallback()
-        false
-      } else if (rightFling(event1, event2, xVelocity, yVelocity)) {
-        rightCallback()
-        false
-      } else {
-        true
+    this(fm : FragmentManager, resolveInfos : List[ResolveInfo], numRows : Int, numCols : Int) = {
+      super(fm)
+      this.resolveInfos = resolveInfos
+      this.numRows = numRows
+      this.numCols = numCols
+    }
+
+    override getCount = {
+      Math.ceil(this.resolveInfos.size / (this.numRows * this.numCols).asInstanceOf[Float]).asInstanceOf[Int]
+    }
+
+    override def getItem(position : Int) = {
+      AppsFragment.newInstance(position, resolveInfos, numRows, numCols)
+    }
+  }
+
+  object AppsFragment {
+    def newInstance(page : Int, resolveInfos : List[ResolveInfo], numRows : Int, numCols : Int) = {
+      (new AppsFragment()).
+        setPage(page).
+        setDimensions(numRows, numCols).
+        setResolveInfos(resolveInfos)
+    }
+  }
+
+
+  class AppsFragment extends Fragment {
+    var page = 0
+    var resolveInfos = new ArrayList[ResolveInfo]()
+    var numRows = 1
+    var numCols = 1
+
+    def setPage(p : Int) = {
+      this.page = p
+      this
+    }
+
+    def setResolveInfos(r : List[ResolveInfo]) = {
+      this.resolveInfos = r
+      this
+    }
+
+    def setDimensions(numRows : Int, numCols : Int) = {
+      this.numRows = numRows
+      this.numCols = numCols
+      this
+    }
+
+    override def onCreateView(inflater : LayoutInflater, container : ViewGroup, savedInstanceState : Bundle) = {
+      val context = getActivity()
+      val packageManager = context.getPackageManager()
+      val cell = inflater.inflate(R.layout.app_item, null, false)
+      var tv
+      var iv
+      val table = new TableLayout(context)
+      var tr
+      var resolveInfo
+      var position
+
+      for (Int rowIndex = 0; rowIndex < numberOfRows(); rowIndex++) {
+        tr = new TableRow(context);
+
+        for (Int columnIndex = 0; columnIndex < numberOfColumns(); columnIndex++) {
+          if (positionIndex(rowIndex, columnIndex) < resolveInfos.size()) {
+            resolveInfo = resolveInfos.get(positionIndex(rowIndex, columnIndex));
+            cell = inflater.inflate(R.layout.app_item, null, false);
+            tv = (TextView)cell.findViewById(R.id.app_name);
+            iv = (ImageView)cell.findViewById(R.id.app_icon);
+
+            tv.setText(resolveInfo.loadLabel(packageManager));
+            iv.setImageDrawable(resolveInfo.loadIcon(packageManager));
+
+            cell.setOnClickListener(new AppOpener(resolveInfo));
+
+            tr.addView(cell);
+          }
+        }
+        table.addView(tr);
+      }
+
+      return table;
+    }
+
+    private Int positionIndex(Int rowIndex, Int columnIndex) {
+      return (this.page * numberOfRows() * numberOfColumns()) +
+        numberOfColumns() * rowIndex + (columnIndex + 1) - 1;
+    }
+
+    private Int numberOfRows() {
+      return numRows;
+    }
+
+    private Int numberOfColumns() {
+      return numCols;
+    }
+
+    class AppOpener implements View.OnClickListener {
+      private ResolveInfo resolveInfo;
+
+      AppOpener(ResolveInfo resolveInfo) {
+        this.resolveInfo = resolveInfo;
+      }
+
+      public void onClick(View v) {
+        Intent i = new Intent();
+
+        i.setClassName(resolveInfo.activityInfo.applicationInfo.packageName,
+            resolveInfo.activityInfo.name);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.setAction(Intent.ACTION_MAIN);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        startActivity(i);
       }
     }
 
-    def onLeft(f : () => Unit) = {
-      leftCallback = f
-      this
-    }
-
-    def onRight(f : () => Unit) = {
-      rightCallback = f
-      this
-    }
-    
-    override def onDown(ignored : MotionEvent) = { true }
-
-    private def leftFling(event1 : MotionEvent, event2 : MotionEvent, xVelocity : Float, yVelocity : Float) = {
-      event1.getX - event2.getX > 120 && xVelocity.abs > 200
-    }
-
-    private def rightFling(event1 : MotionEvent, event2 : MotionEvent, xVelocity : Float, yVelocity : Float) = {
-      event2.getX - event1.getX > 120 && xVelocity.abs > 200
-    }
   }
 }
