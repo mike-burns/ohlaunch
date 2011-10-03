ab27524e6ffbf7ba3212d548835d936e3c62833b
Mike Burns <mike@mike-burns.com>
2011-10-03 19:21:41 -0400

    Add an icon

--- src/main/java/com/mike_burns/ohlaunch/R.java
+++ src/main/java/com/mike_burns/ohlaunch/R.java
@@ -10,18 +10,21 @@ package com.mike_burns.ohlaunch;
 public final class R {
     public static final class attr {
     }
+    public static final class drawable {
+        public static final int icon=0x7f020000;
+    }
     public static final class id {
-        public static final int app_icon=0x7f040000;
-        public static final int app_name=0x7f040001;
-        public static final int paginatorizer=0x7f040002;
+        public static final int app_icon=0x7f050000;
+        public static final int app_name=0x7f050001;
+        public static final int paginatorizer=0x7f050002;
     }
     public static final class layout {
-        public static final int app_item=0x7f020000;
-        public static final int main=0x7f020001;
+        public static final int app_item=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int activity_list_empty=0x7f030001;
-        public static final int activity_list_loading=0x7f030002;
-        public static final int app_name=0x7f030000;
+        public static final int activity_list_empty=0x7f040001;
+        public static final int activity_list_loading=0x7f040002;
+        public static final int app_name=0x7f040000;
     }
 }
